4d83607edd5c89eaa84503e5711fc890d557b465
Aleksandr Svatkov <saealeksandrsae@mail.ru>
2023-02-18 23:13:23 +0300

    Editing profile form

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-let page = document.querySelector('.page');
-let content = page.querySelector('.content');
+const page = document.querySelector('.page');
+const content = page.querySelector('.content');
 
-let editBtn = content.querySelector('.profile__edit-button');
-let addBtn = content.querySelector('.profile__add-button');
+const editBtn = content.querySelector('.profile__edit-button');
+
+const editForm = page.querySelector('.popup__form_type_profile');
+const namePopup = editForm.querySelector('.popup__input_text_name');
+const activityPopup = editForm.querySelector('.popup__input_text_activity');
+const nameProfile = content.querySelector('.profile__name');
+const activityProfile = content.querySelector('.profile__activity');
+
+function autofillFormInput() {
+  namePopup.value = nameProfile.textContent;
+  activityPopup.value = activityProfile.textContent;
+}
 
 editBtn.addEventListener('click', function () {
   let popup = page.querySelector('.popup_type_profile');
   popup.style = 'visibility: visible; opacity: 1;';
+
+  autofillFormInput();
 })
 
-addBtn.addEventListener('click', function () {
-  let popup = page.querySelector('.popup_type_place');
-  popup.style = 'visibility: visible; opacity: 1;';
-})
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
+  nameProfile.textContent = namePopup.value;
+  activityProfile.textContent = activityPopup.value;
+}
 
-let closeEditBtn = page.querySelector('.popup_type_profile .popup__close-button');
-let closeAddBtn = page.querySelector('.popup_type_place .popup__close-button');
+editForm.addEventListener('submit', handleFormSubmit);
 
+const closeEditBtn = page.querySelector('.popup_type_profile .popup__close-button');
 closeEditBtn.addEventListener('click', function () {
   let popup = page.querySelector('.popup_type_profile');
   popup.style = 'visibility: hidden; opacity: 0;';
 })
 
+
+const addBtn = content.querySelector('.profile__add-button');
+addBtn.addEventListener('click', function () {
+  let popup = page.querySelector('.popup_type_place');
+  popup.style = 'visibility: visible; opacity: 1;';
+})
+
+const closeAddBtn = page.querySelector('.popup_type_place .popup__close-button');
 closeAddBtn.addEventListener('click', function () {
   let popup = page.querySelector('.popup_type_place');
   popup.style = 'visibility: hidden; opacity: 0;';
